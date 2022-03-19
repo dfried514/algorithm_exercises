23165fcec238b782b35134f376457f681f070d40
dfried514 <dfried514@gmail.com>
2022-03-18 23:51:03 -0700

    incomplete

---
+++ dp/levenshteinDistance.js
@@ -0,0 +1,20 @@
+function levenshteinDistance(str1, str2) {
+	const dp = [];
+	for (let i = 0; i <= str1.length; i++) {
+		dp[i] = [i];
+	}
+	for (let j = 0; j <= str2.length; j++) {
+		dp[0][j] = j; 
+	}
+	console.log(str1, str2, dp);
+	for (let i = 0; i < str1.length; i++) {
+		for (let j = 0; j < str2.length; j++) {
+			dp[i+1][j+1] = Math.min(dp[i][j], dp[i][j+1], dp[i+1][j]) + 1;
+			if (str1.charAt(i) == str2.charAt(j) && dp[i+1][j+1] > 0) 
+				dp[i+1][j+1]--;
+		}
+	}
+	console.log(dp);
+	return dp[str1.length][str2.length];
+}
+
