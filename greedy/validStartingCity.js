786df16963dd5f119f460ca30a3ba5d6cd5d8551
dfried514 <dfried514@gmail.com>
2022-03-25 23:40:10 -0700

    incomplete 2

--- greedy/validStartingCity.js
+++ greedy/validStartingCity.js
@@ -1,13 +1,17 @@
 function validStartingCity(distances, fuel, mpg) {
-	let totalDistance = totalFuel = start = 0;
-	for (let i = 0; i < distances.length; i++) {
-		let curFuel = fuel[i] * mpg;
-		totalFuel += curFuel;
-		totalDistance += distances[i];
+	let idx = 1, start = totalDistance = totalFuel = 0;
+	while (idx <= distances.length) {
+		while (distances[idx - 1] < fuel[idx - 1] * mpg) {
+			idx++;
+		}
+		if (idx == distances.length) return start;
+		start = idx;
+		totalDistance += distances[idx - 1];
+		totalFuel += (fuel[idx - 1] * mpg);
 		
-		if (totalFuel < totalDistance && curFuel < distances[i]) 
-			start = (i + 1) % distances.length;
-	}
-	return totalFuel >= totalDistance ? start : -1;
+		if (totalDistance < totalFuel) {
+			totalDistance = 0;
+			totalFuel = 0;
+		}
+		
 }
-
